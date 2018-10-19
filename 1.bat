964bb8ce1f29739a5655195504b571f2c8c66dca
xuetuing <wangtjun@yahoo.com>
2018-10-19 18:00:06 +0800

    ！！

--- 1.bat
+++ 1.bat
@@ -3,6 +3,6 @@ set "d1=d:\a"
 set "d2=d:\b"
 cd /d %d1%
 for /f "delims=" %%a in ('dir/b "%d1%\*"') do (
-if not exist "%d2%\%%~nxa" xcopy /y/e "%%a" "%d2%\%%~nxa"
+  if not exist "%d2%\%%~nxa" xcopy /y/e/s/i "%%a" "%d2%\"
 )
 pause
