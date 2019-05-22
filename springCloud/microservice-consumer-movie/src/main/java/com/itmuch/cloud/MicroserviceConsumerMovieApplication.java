29f993d357856c385ffc5a53bf2034b3ed0bfe70
xuetuing <32641642+xuetuing@users.noreply.github.com>
2019-05-22 20:31:24 +0800

    spring cloud eureka

--- springCloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceConsumerMovieApplication.java
+++ springCloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceConsumerMovieApplication.java
@@ -1,11 +1,12 @@
 package com.itmuch.cloud;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class MicroserviceSimpleConsumerMovieApplication {
+public class MicroserviceConsumerMovieApplication {
 
     @Bean
     public RestTemplate restTemplate(){
@@ -13,7 +14,7 @@ public class MicroserviceSimpleConsumerMovieApplication {
     }
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+		SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
 	}
 
 }
--- springCloud/microservice-provider-user/src/main/java/com/itmuch/cloud/MicroserviceProviderUserApplication.java
+++ springCloud/microservice-provider-user/src/main/java/com/itmuch/cloud/MicroserviceProviderUserApplication.java
@@ -2,12 +2,14 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class MicroserviceSimpleProviderUserApplication {
+@EnableEurekaClient
+public class MicroserviceProviderUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceSimpleProviderUserApplication.class, args);
+		SpringApplication.run(MicroserviceProviderUserApplication.class, args);
 	}
 
 }
